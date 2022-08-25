03f56679b7c475c565b47cb48440d6b7e3b88f45
David Chao <davidchao@google.com>
2022-08-25 16:13:59 +0800

    thermal: seperate thermal config for charging
    Separate thermal config for charger mode and remove the unused thermal
    throttling.
    
    Bug: 243347663
    Test: thermalHAL init completed in charger.
    Change-Id: I474d58d51ec715c90fcf4d8ad8d377db4e8e43f0

--- device-tangorpro.mk
+++ device-tangorpro.mk
@@ -70,7 +70,8 @@ PRODUCT_COPY_FILES += \
 
 # Thermal Config
 PRODUCT_COPY_FILES += \
-	device/google/tangorpro/thermal_info_config_tangorpro.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+	device/google/tangorpro/thermal_info_config_tangorpro.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
+	device/google/tangorpro/thermal_info_config_charge_tangorpro.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_charge.json \
 
 # Power HAL config
 PRODUCT_COPY_FILES += \
