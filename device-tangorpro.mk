a1599cceaf15b6d1f8c39bb15a426dbf78290e2c
flawedworld <flawedworld@flawed.world>
2022-10-15 19:25:55 +0100

    Add sysconfig from stock OS
    Co-Authored-By: Vladislav <stvlad06@gmail.com>

--- device-tangorpro.mk
+++ device-tangorpro.mk
@@ -320,3 +320,7 @@ PRODUCT_COPY_FILES += \
 # SKU specific RROs
 PRODUCT_PACKAGES += \
     SettingsOverlayGTU8P
+
+# sysconfig XML from stock
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
