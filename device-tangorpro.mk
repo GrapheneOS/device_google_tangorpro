aad7b0f11465c7be56584b073b34fbd162ac1936
Adam Shih <adamshih@google.com>
2023-02-13 12:46:18 +0800

    include led dump
    Bug: 240530709
    Test: adb bugreport
    Change-Id: Iaaaa59268269645f5d712c86935f5917491658a9

--- device-tangorpro.mk
+++ device-tangorpro.mk
@@ -53,6 +53,7 @@ include device/google/tangorpro/audio/tangorpro/audio-tables.mk
 include device/google/gs201/device-shipping-common.mk
 include device/google/gs-common/touch/gti/gti.mk
 include device/google/gs-common/touch/nvt/nvt.mk
+include device/google/gs-common/led/led.mk
 
 # go/lyric-soong-variables
 $(call soong_config_set,lyric,camera_hardware,tangorpro)
