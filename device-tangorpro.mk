ab6182c543c00482b8ef97a68ff6865ef08977ed
flawedworld <flawedworld@flawed.world>
2023-06-21 20:23:51 +0000

    SystemUITitan -> SystemUI

--- device-tangorpro.mk
+++ device-tangorpro.mk
@@ -71,7 +71,7 @@ include device/google/tangorpro/uwb/uwb_calibration.mk
 endif
 
 # Preopt SystemUI
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUITitan  # For tablet
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For tablet
 
 # Touch files
 PRODUCT_COPY_FILES += \
