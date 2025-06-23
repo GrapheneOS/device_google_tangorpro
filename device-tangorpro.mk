50f7abb96dcc55dda35f5049931d87ff2ce37a49
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 16:07:40 +0000

    strip display color data files

--- device-tangorpro.mk
+++ device-tangorpro.mk
@@ -203,11 +203,6 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
 
-# Display Config
-PRODUCT_COPY_FILES += \
-        device/google/tangorpro/tangorpro/display_golden_boe-ts110f5mlg0-rt4_cal0.pb:$(TARGET_COPY_OUT_VENDOR)/etc/display_golden_boe-ts110f5mlg0-rt4_cal0.pb \
-        device/google/tangorpro/tangorpro/display_golden_csot-ppa957db2d-rt4_cal0.pb:$(TARGET_COPY_OUT_VENDOR)/etc/display_golden_csot-ppa957db2d-rt4_cal0.pb
-
 # Enable HWC dynamic recomposition for display with index 0
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += vendor.display.dynamic_recomposition=1
 
